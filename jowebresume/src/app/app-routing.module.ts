1c121cf0901965030b4c43e42687eaa8e392b07d
Jo <jdheinen@yahoo.com>
2019-07-29 17:55:28 +1000

    Landing page

--- jowebresume/src/app/app-routing.module.ts
+++ jowebresume/src/app/app-routing.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExperienceComponent } from './experience/experience.component';
+import { SkillsComponent } from './skills/skills.component';
+import { HobbiesComponent } from './hobbies/hobbies.component';
+import { DegreesComponent } from './degrees/degrees.component';
 
 const routes: Routes = [
+  { path: 'experience', component: ExperienceComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'hobbies', component: HobbiesComponent},
+  { path: 'degrees', component: DegreesComponent },
+
+
+
   { path: '', redirectTo: '/', pathMatch: 'full' },
   { path: '**', redirectTo: '/', pathMatch: 'full' }
+
 ];
 
 export const AppRoutingHash = RouterModule.forRoot(routes, { useHash: true });
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes, { enableTracing: false }
+)
   ],
   exports: [RouterModule]
 })
--- jowebresume/src/app/app.module.ts
+++ jowebresume/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppRoutingHash } from './app-routing.module';
+import { ExperienceComponent } from './experience/experience.component';
+import { SkillsComponent } from './skills/skills.component';
+import { HobbiesComponent } from './hobbies/hobbies.component';
+import { DegreesComponent } from './degrees/degrees.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExperienceComponent,
+    SkillsComponent,
+    HobbiesComponent,
+    DegreesComponent
   ],
   imports: [
     BrowserModule,
